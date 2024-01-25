5c8be104ccccacf02fd1d30d6a92e09596cf7572
Anju Sebastian <christeenaanju@gmail.com>
2024-01-25 19:54:00 +0530

    path uploaded

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -2,7 +2,7 @@ const multer=require("multer");
 const path=require("path");
 const storage=multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,'./uploads/restaurantPhotograps')
+        cb(null,__dirname + '../../uploads/restaurantPhotograps')
     },
     filename:function(req,file,cb){
         cb(null,Date.now()+path.extname(file.originalname));
